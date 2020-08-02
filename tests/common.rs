80aba49e37dcf1a489c0eccd98a780185251140e
Paolo Barbolini <paolo@paolo565.org>
2020-08-02 17:37:28 +0200

    Improve tests after the refactor from #18

--- tests/common.rs
+++ tests/common.rs
@@ -1,32 +1,34 @@
 #[macro_export]
 macro_rules! test_format {
-    ($exp_matchert:expr, $exp_mimet:expr, $exp_ext:expr, $path_name:ident, $path_embed:ident, $file:expr) => {
-        #[cfg(feature = "std")]
-        #[test]
-        fn $path_name() {
+    ($exp_matchert:ident, $exp_mimet:expr, $exp_ext:expr, $format:ident, $file:expr) => {
+        mod $format {
+            use infer::{MatcherType, Type};
+
             fn matcher(_buf: &[u8]) -> bool {
                 false
             }
 
-            let expected_kind = Type::new($exp_matchert, $exp_mimet, $exp_ext, matcher);
-            let kind = infer::get_from_path(concat!("testdata/", $file))
-                .expect("test file read")
-                .expect("test file matches");
+            #[cfg(feature = "std")]
+            #[test]
+            fn get_from_path() {
+                let expected_kind =
+                    Type::new(MatcherType::$exp_matchert, $exp_mimet, $exp_ext, matcher);
+                let kind = infer::get_from_path(concat!("testdata/", $file))
+                    .expect("test file read")
+                    .expect("test file matches");
 
-            assert_eq!(expected_kind, kind);
-        }
-
-        #[test]
-        fn $path_embed() {
-            fn matcher(_buf: &[u8]) -> bool {
-                false
+                assert_eq!(expected_kind, kind);
             }
 
-            let expected_kind = Type::new($exp_matchert, $exp_mimet, $exp_ext, matcher);
-            let buf = include_bytes!(concat!("../testdata/", $file));
-            let kind = infer::get(buf).expect("test file matches");
+            #[test]
+            fn get() {
+                let expected_kind =
+                    Type::new(MatcherType::$exp_matchert, $exp_mimet, $exp_ext, matcher);
+                let buf = include_bytes!(concat!("../testdata/", $file));
+                let kind = infer::get(buf).expect("test file matches");
 
-            assert_eq!(expected_kind, kind);
+                assert_eq!(expected_kind, kind);
+            }
         }
     };
 }
--- tests/doc.rs
+++ tests/doc.rs
@@ -1,39 +1,27 @@
-use infer::{MatcherType, Type};
-
 mod common;
 
-test_format!(
-    MatcherType::DOC,
-    "application/msword",
-    "doc",
-    test_doc,
-    test_doc_embed,
-    "sample.doc"
-);
+test_format!(DOC, "application/msword", "doc", doc, "sample.doc");
 
 test_format!(
-    MatcherType::DOC,
+    DOC,
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     "docx",
-    test_docx,
-    test_docx_embed,
+    docx,
     "sample.docx"
 );
 
 test_format!(
-    MatcherType::DOC,
+    DOC,
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     "xlsx",
-    test_xlsx,
-    test_xlsx_embed,
+    xlsx,
     "sample.xlsx"
 );
 
 test_format!(
-    MatcherType::DOC,
+    DOC,
     "application/application/vnd.openxmlformats-officedocument.presentationml.presentation",
     "pptx",
-    test_pptx,
-    test_pptx_embed,
+    pptx,
     "sample.pptx"
 );
