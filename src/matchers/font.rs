a80c9bd97998270cbbf9ec3441993703aeef99d9
zhuoxian.dzx <zhuoxian.dzx@alibaba-inc.com>
2022-07-13 15:57:17 +0800

    fix matcher for woff & woff2

--- src/matchers/font.rs
+++ src/matchers/font.rs
@@ -1,27 +1,19 @@
 /// Returns whether a buffer is WOFF font data.
 pub fn is_woff(buf: &[u8]) -> bool {
-    buf.len() > 7
+    buf.len() > 3
         && buf[0] == 0x77
         && buf[1] == 0x4F
         && buf[2] == 0x46
         && buf[3] == 0x46
-        && buf[4] == 0x00
-        && buf[5] == 0x01
-        && buf[6] == 0x00
-        && buf[7] == 0x00
 }
 
 /// Returns whether a buffer is WOFF2 font data.
 pub fn is_woff2(buf: &[u8]) -> bool {
-    buf.len() > 7
+    buf.len() > 3
         && buf[0] == 0x77
         && buf[1] == 0x4F
         && buf[2] == 0x46
         && buf[3] == 0x32
-        && buf[4] == 0x00
-        && buf[5] == 0x01
-        && buf[6] == 0x00
-        && buf[7] == 0x00
 }
 
 /// Returns whether a buffer is TTF font data.
