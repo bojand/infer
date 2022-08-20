a1186700d4569d28e49a41b667f41f01ac183f95
Rafał Mikrut <mikrutrafal@protonmail.com>
2022-08-20 09:44:31 +0200

    Add support for PEM files

--- tests/app.rs
+++ tests/app.rs
@@ -45,3 +45,5 @@ test_format!(
 test_format!(App, "application/wasm", "wasm", wasm, "sample.wasm");
 
 test_format!(App, "application/x-x509-ca-cert", "der", der, "sample.der");
+
+test_format!(App, "application/x-x509-ca-cert", "pem", pem, "sample.pem");
