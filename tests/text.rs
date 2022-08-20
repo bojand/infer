467e2a32c36ec79f762c3cb1b91d6e349a8b0135
Rafał Mikrut <mikrutrafal@protonmail.com>
2022-08-20 08:46:44 +0200

    Remove BOM coding from xml files

--- tests/text.rs
+++ tests/text.rs
@@ -4,4 +4,6 @@ test_format!(Text, "text/html", "html", html, "sample.html");
 
 test_format!(Text, "text/xml", "xml", xml, "sample.xml");
 
+test_format!(Text, "text/xml", "xml", xml2, "sample2.xml");
+
 test_format!(Text, "text/x-shellscript", "sh", sh, "sample.sh");
