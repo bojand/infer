7625babcdfe3ae09bf7f0326dc662189d6387e57
Bojan <dbojan@gmail.com>
2022-09-18 20:59:34 -0300

    add read support to main api

--- src/lib.rs
+++ src/lib.rs
@@ -181,14 +181,13 @@ impl Infer {
         let mut res_value: Option<Type> = None;
 
         for kind in self.iter_matchers() {
-            if res_value.is_none() {
-                let match_res = kind.matches_read(r)?;
-                if match_res {
-                    res_value = Some(*kind);
-                }
-
-                r.rewind().ok();
+            let match_res = kind.matches_read(r)?;
+            if match_res {
+                res_value = Some(*kind);
+                break;
             }
+
+            r.rewind().ok();
         }
 
         Ok(res_value)
@@ -223,6 +222,32 @@ impl Infer {
             .any(|kind| kind.extension() == extension && kind.matches(buf))
     }
 
+    /// Determines whether data from read is of given extension.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_read`](./fn.is_read.html).
+    pub fn is_read<R>(&self, r: &mut R, extension: &str) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        let mut res_value: bool = false;
+
+        for kind in self.iter_matchers() {
+            if kind.extension() == extension {
+                let match_res = kind.matches_read(r)?;
+                if match_res {
+                    res_value = true;
+                    break;
+                }
+
+                r.rewind().ok();
+            }
+        }
+
+        Ok(res_value)
+    }
+
     /// Determines whether a buffer is of given mime type.
     ///
     /// # Examples
@@ -231,6 +256,31 @@ impl Infer {
     pub fn is_mime(&self, buf: &[u8], mime_type: &str) -> bool {
         self.iter_matchers()
             .any(|kind| kind.mime_type() == mime_type && kind.matches(buf))
+    }
+
+    /// Determines whether data from reader is of given mime type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_mime_read`](./fn.is_mime_read.html).
+    pub fn is_mime_read<R>(&self, r: &mut R, mime_type: &str) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        let mut res_value: bool = false;
+        for kind in self.iter_matchers() {
+            if kind.mime_type() == mime_type {
+                let match_res = kind.matches_read(r)?;
+                if match_res {
+                    res_value = true;
+                    break;
+                }
+
+                r.rewind().ok();
+            }
+        }
+
+        Ok(res_value)
     }
 
     /// Returns whether an extension is supported.
@@ -284,6 +334,18 @@ impl Infer {
         self.is_type(buf, MatcherType::App)
     }
 
+    /// Determines whether data is an application type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_app_read`](./fn.is_app_read.html).
+    pub fn is_app_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::App)
+    }
+
     /// Determines whether a buffer is an archive type.
     ///
     /// # Examples
@@ -291,6 +353,18 @@ impl Infer {
     /// See [`is_archive`](./fn.is_archive.html).
     pub fn is_archive(&self, buf: &[u8]) -> bool {
         self.is_type(buf, MatcherType::Archive)
+    }
+
+    /// Determines whether data from reader is an archive type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_archive_read`](./fn.is_archive_read.html).
+    pub fn is_archive_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Archive)
     }
 
     /// Determines whether a buffer is an audio type.
@@ -302,6 +376,18 @@ impl Infer {
         self.is_type(buf, MatcherType::Audio)
     }
 
+    /// Determines whether data from reader is an audio type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_audio_read`](./fn.is_audio_read.html).
+    pub fn is_audio_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Audio)
+    }
+
     /// Determines whether a buffer is a book type.
     ///
     /// # Examples
@@ -309,6 +395,18 @@ impl Infer {
     /// See [`is_book`](./fn.is_book.html).
     pub fn is_book(&self, buf: &[u8]) -> bool {
         self.is_type(buf, MatcherType::Book)
+    }
+
+    /// Determines whether data from reader is a book type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_book_read`](./fn.is_book_read.html).
+    pub fn is_book_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Book)
     }
 
     /// Determines whether a buffer is a document type.
@@ -320,6 +418,18 @@ impl Infer {
         self.is_type(buf, MatcherType::Doc)
     }
 
+    /// Determines whether data from reader is a document type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_document_read`](./fn.is_document_read.html).
+    pub fn is_document_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Doc)
+    }
+
     /// Determines whether a buffer is a font type.
     ///
     /// # Examples
@@ -327,6 +437,18 @@ impl Infer {
     /// See [`is_font`](./fn.is_font.html).
     pub fn is_font(&self, buf: &[u8]) -> bool {
         self.is_type(buf, MatcherType::Font)
+    }
+
+    /// Determines whether data from reader is a font type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_font_read`](./fn.is_font_read.html).
+    pub fn is_font_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Font)
     }
 
     /// Determines whether a buffer is an image type.
@@ -338,6 +460,18 @@ impl Infer {
         self.is_type(buf, MatcherType::Image)
     }
 
+    /// Determines whether data from reader is an image type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_image_read`](./fn.is_image_read.html).
+    pub fn is_image_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Image)
+    }
+
     /// Determines whether a buffer is a video type.
     ///
     /// # Examples
@@ -345,6 +479,18 @@ impl Infer {
     /// See [`is_video`](./fn.is_video.html).
     pub fn is_video(&self, buf: &[u8]) -> bool {
         self.is_type(buf, MatcherType::Video)
+    }
+
+    /// Determines whether data from reader is a video type.
+    ///
+    /// # Examples
+    ///
+    /// See [`is_video_read`](./fn.is_video_read.html).
+    pub fn is_video_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Video)
     }
 
     /// Determines whether a buffer is one of the custom types added.
@@ -366,6 +512,14 @@ impl Infer {
     /// ```
     pub fn is_custom(&self, buf: &[u8]) -> bool {
         self.is_type(buf, MatcherType::Custom)
+    }
+
+    /// Determines whether data from reader is one of the custom types added.
+    pub fn is_custom_read<R>(&self, r: &mut R) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        self.is_type_read(r, MatcherType::Custom)
     }
 
     /// Adds a custom matcher.
@@ -408,6 +562,27 @@ impl Infer {
     fn is_type(&self, buf: &[u8], matcher_type: MatcherType) -> bool {
         self.iter_matchers()
             .any(|kind| kind.matcher_type() == matcher_type && kind.matches(buf))
+    }
+
+    fn is_type_read<R>(&self, r: &mut R, matcher_type: MatcherType) -> io::Result<bool>
+    where
+        R: Read + Seek,
+    {
+        let mut res_value: bool = false;
+
+        for kind in self.iter_matchers() {
+            if kind.matcher_type() == matcher_type && kind.supports_read_match() {
+                let match_res = kind.matches_read(r)?;
+                if match_res {
+                    res_value = true;
+                    break;
+                }
+
+                r.rewind().ok();
+            }
+        }
+
+        Ok(res_value)
     }
 }
 
@@ -491,6 +666,29 @@ pub fn is(buf: &[u8], extension: &str) -> bool {
     INFER.is(buf, extension)
 }
 
+/// Determines whether a buffer is of given extension.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.jpg")?;
+///     let jpg = infer::is_read(&mut f, "jpg").unwrap();
+///     assert!(jpg);
+///     Ok(())
+/// }
+/// ```
+pub fn is_read<R>(r: &mut R, extension: &str) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_read(r, extension)
+}
+
 /// Determines whether a buffer is of given mime type.
 ///
 /// # Examples
@@ -501,6 +699,28 @@ pub fn is(buf: &[u8], extension: &str) -> bool {
 /// ```
 pub fn is_mime(buf: &[u8], mime_type: &str) -> bool {
     INFER.is_mime(buf, mime_type)
+}
+
+/// Determines whether data from reader is of given mime type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.jpg")?;
+///     assert!(infer::is_mime_read(&mut f, "image/jpeg").unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_mime_read<R>(r: &mut R, mime_type: &str) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_mime_read(r, mime_type)
 }
 
 /// Returns whether an extension is supported.
@@ -537,6 +757,28 @@ pub fn is_app(buf: &[u8]) -> bool {
     INFER.is_app(buf)
 }
 
+/// Determines whether data from reader is an application type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.wasm")?;
+///     assert!(infer::is_app_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_app_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_app_read(r)
+}
+
 /// Determines whether a buffer is an archive type.
 /// # Examples
 ///
@@ -546,6 +788,27 @@ pub fn is_app(buf: &[u8]) -> bool {
 /// ```
 pub fn is_archive(buf: &[u8]) -> bool {
     INFER.is_archive(buf)
+}
+
+/// Determines whether data from reader is an archive type.
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.pdf")?;
+///     assert!(infer::is_archive_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_archive_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_archive_read(r)
 }
 
 /// Determines whether a buffer is an audio type.
@@ -561,6 +824,28 @@ pub fn is_audio(buf: &[u8]) -> bool {
     INFER.is_audio(buf)
 }
 
+/// Determines whether data from reader is an audio type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.mp3")?;
+///     assert!(infer::is_audio_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_audio_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_audio_read(r)
+}
+
 /// Determines whether a buffer is a book type.
 ///
 /// # Examples
@@ -571,6 +856,28 @@ pub fn is_audio(buf: &[u8]) -> bool {
 /// ```
 pub fn is_book(buf: &[u8]) -> bool {
     INFER.is_book(buf)
+}
+
+/// Determines whether data from buffer is a book type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.epub")?;
+///     assert!(infer::is_book_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_book_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_book_read(r)
 }
 
 /// Determines whether a buffer is a document type.
@@ -585,6 +892,28 @@ pub fn is_document(buf: &[u8]) -> bool {
     INFER.is_document(buf)
 }
 
+/// Determines whether data from reader is a document type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.docx")?;
+///     assert!(infer::is_document_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_document_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_document_read(r)
+}
+
 /// Determines whether a buffer is a font type.
 ///
 /// # Examples
@@ -595,6 +924,28 @@ pub fn is_document(buf: &[u8]) -> bool {
 /// ```
 pub fn is_font(buf: &[u8]) -> bool {
     INFER.is_font(buf)
+}
+
+/// Determines whether data from reader is a font type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.ttf")?;
+///     assert!(infer::is_font_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_font_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_font_read(r)
 }
 
 /// Determines whether a buffer is an image type.
@@ -609,6 +960,28 @@ pub fn is_image(buf: &[u8]) -> bool {
     INFER.is_image(buf)
 }
 
+/// Determines whether data from reader is an image type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.png")?;
+///     assert!(infer::is_image_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_image_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_image_read(r)
+}
+
 /// Determines whether a buffer is a video type.
 ///
 /// # Examples
@@ -619,6 +992,28 @@ pub fn is_image(buf: &[u8]) -> bool {
 /// ```
 pub fn is_video(buf: &[u8]) -> bool {
     INFER.is_video(buf)
+}
+
+/// Determines whether data from reader is a video type.
+///
+/// # Examples
+///
+/// ```rust
+/// use std::fs;
+/// use std::io::prelude::*;
+/// use std::fs::File;
+///
+/// fn main() -> std::io::Result<()> {
+///     let mut f = File::open("testdata/sample.mov")?;
+///     assert!(infer::is_video_read(&mut f).unwrap());
+///     Ok(())
+/// }
+/// ```
+pub fn is_video_read<R>(r: &mut R) -> io::Result<bool>
+where
+    R: Read + Seek,
+{
+    INFER.is_video_read(r)
 }
 
 /// Returns the file type for the mime type if supported.
